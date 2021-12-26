4bf4e6a780ac99b6656da270afb461db9cfbbbbc
JoseOsejo <80367652+JoseOsejo@users.noreply.github.com>
2021-12-26 12:21:22 -0600

    subiendo Battleship

---
+++ src/battleship/Battleship.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jcoq2
+ */
+public class Battleship {
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        /*System.out.println("Ingrese Cantidad de Nombres: ");
+        int cantidadNombres = leer.nextInt();
+        String[] nombres;
+        nombres = new String [cantidadNombres];
+        for(int i = 0; i<nombres.length;i++){
+            nombres[i] = leer.next();
+        }
+        for(String nombre : nombres){
+            System.out.print(nombre+" ");
+        }*/
+        
+        //Battleship.Imprimirnombres(4,leer);
+    }
+
+}
